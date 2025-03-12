a37c18ec6a77d304297e56412689e99edcdf6a8f
jackp2309 <jackp2309@gmail.com>
2025-03-11 21:49:11 -0500

    Methods set up and inputs set up

--- Main.java
+++ Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.print("Please provide your risk tolerance(high, medium, low): ");
+        String riskTolerance = scan.nextLine();
+        scan.nextLine();
+
+        System.out.print("Please provide your monthly income: ");
+        long monthlyIncome = scan.nextLong();
+        scan.nextLine();
+
+        System.out.print("Please provide your time horizon: ");
+        double timeHorizon = scan.nextDouble();
+        scan.nextLine();
+
     }
+
+    private static void calcRiskScore(){
+        System.out.println("Calculating Risk Score...");
+    }
+    private static void calcTimeScore(){
+        System.out.println("Calculating Time Score...");
+    }
+    private static void calcTaxBracket(){
+        System.out.println("Calculating Tax Bracket...");
+    }
+    private static void calcAllocation(){
+        System.out.println("Calculating Allocation...");
+    }
+
 }
