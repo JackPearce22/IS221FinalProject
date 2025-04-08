72cd64b5c074ecceff864b426da3373b436ed9ce
dannylack <dannylack9@gmail.com>
2025-04-08 12:14:41 -0500

    I added Smart Wealth Allocator print statement

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ public class Main {
 
     public static void main(String[] args){
 
+            System.out.print("***Welcome to Smart Wealth Allocator***");
+            scan.nextLine();
+
             System.out.print("Please provide your risk tolerance(high, medium, low): ");
             String riskTolerance = scan.nextLine();
 
