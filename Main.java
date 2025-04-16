98da97f1d68b4295c1fd472b644b96a6e1ee3edb
jackp2309 <jackp2309@gmail.com>
2025-04-15 20:48:26 -0500

    Added printResults method

--- Main.java
+++ Main.java
@@ -51,10 +51,11 @@ public class Main {
             System.out.print("Please provide your monthly expenses: ");
             double monthlyExpenses = scan.nextDouble();
 
-            calcAllocation(riskScore, timeScore);
+            String allocationType = calcAllocation(riskScore, timeScore);
 
-            double minEmergencyFund = monthlyExpenses * 3;
-            double maxEmergencyFund = monthlyExpenses * 6;
+            String result = printResult(annualIncome,monthlyExpenses, allocationType);
+            System.out.print(result);
+            System.out.print("Disclaimer: This tool is for informational purposes only and does not constitute financial, investment, or tax advice. Please consult a licensed financial advisor or tax professional before making any financial decisions. ");
 
     }
 
@@ -136,7 +137,7 @@ public class Main {
             System.out.println("Your annual income is $" + annualIncome);
             System.out.println("Your estimated tax bracket is: " + (int)taxRate + "%");
         }
-        private static void calcAllocation (int riskScore, int timeScore){
+        private static String calcAllocation (int riskScore, int timeScore){
             System.out.println("Calculating Allocation...");
 
             int allocationScore = riskScore * timeScore;
@@ -145,19 +146,89 @@ public class Main {
 
             if (allocationScore >= 825 && allocationScore <= 1650) {
                 System.out.println("Recommended Allocation: Conservative");
+                return "Conservative";
             } else if (allocationScore >= 2475 && allocationScore <= 2500) {
                 System.out.println("Recommended Allocation: Moderately Conservative");
+                return "Moderately Conservative";
             } else if (allocationScore >= 3300 && allocationScore <= 5000) {
                 System.out.println("Recommended Allocation: Moderate Allocation");
+                return "Moderate Allocation";
             } else if (allocationScore >= 6600 && allocationScore <= 7500) {
                 System.out.println("Recommended Allocation: Moderately Aggressive");
+                return "Moderately Aggressive";
             } else if (allocationScore == 10000) {
                 System.out.println("Recommended Allocation: Aggressive");
+                return "Aggressive";
             } else {
                 System.out.println("Unable to determine a precise allocation. Please review your inputs.");
+                return "Unable to determine a precise allocation. Please review your inputs.";
             }
 
 
+
+        }
+
+        private static String printResult(long annualIncome, double monthlyExpenses, String allocationType){
+            double minEmergencyFund = monthlyExpenses * 3;
+            double maxEmergencyFund = monthlyExpenses * 6;
+
+            double retirementPlan = annualIncome * .05;
+            double brokerageAccount = annualIncome * .1;
+
+            String rothFunds = "";
+            String portfolioBreakdown = "";
+
+            if (allocationType == "Conservative"){
+                rothFunds += "conservative growth (WBALX, VWINX, FXNAX )";
+                portfolioBreakdown += "10% Large cap stocks\n" +
+                        "15 % Small Cap\n" +
+                        "15% International\n" +
+                        "50% Fixed Income\n" +
+                        "10% Cash\n";
+            } else if (allocationType == "Moderately Conservative") {
+                rothFunds += "moderately conservative growth (VWINX, FXNAX, VFSTX )";
+                portfolioBreakdown += "15% Large cap stocks\n" +
+                        "20 % Small Cap\n" +
+                        "20% International\n" +
+                        "35% Fixed Income\n" +
+                        "10% Cash\n";
+
+            } else if (allocationType == "Moderate Allocation") {
+                rothFunds += "moderate allocation growth (FSKAX, VBIAX, FXNAX)";
+                portfolioBreakdown += "10% Large cap stocks\n" +
+                        "15 % Small Cap\n" +
+                        "15% International\n" +
+                        "50% Fixed Income\n" +
+                        "10% Cash\n";
+
+            } else if (allocationType == "Moderately Aggressive") {
+                rothFunds += "moderately aggressive growth (VTI, FTIHX, VWELX)";
+                portfolioBreakdown += "50% Large cap stocks\n" +
+                        "20% Small Cap\n" +
+                        "25% International\n" +
+                        "5% Cash\n";
+
+            } else if (allocationType == "Aggressive") {
+                rothFunds += "aggressive growth (VOOG, SMH, QQQM )";
+                portfolioBreakdown += "50% Large cap stocks\n" +
+                        "20 % Small Cap\n" +
+                        "25% International\n" +
+                        "5% Cash\n";
+
+            } else{
+                System.out.println("Error");
+            }
+
+            return "Emergency Fund (3 - 6 months of expenses) in High Yield Savings Account\n" +
+                    "This would equal ~"+minEmergencyFund+"-"+maxEmergencyFund+"\n" +
+                    "CIT bank offers a High-Yield Savings Account offering variable 4.10% Annual Percentage Yield with a minimum $5,000 balance, but interest may change at any time\n" +
+                    "Retirement Plan (5%)\n" +
+                    "Choose Roth 401k if given the option for company plan\n" +
+                    "5% of income to match company benefits = "+retirementPlan+" per year\n" +
+                    "Funds to consider in Roth IRA for "+rothFunds+"\n" +
+                    "Brokerage Account (10% of income)\n" +
+                    "10% of income = "+brokerageAccount+" per year\n" +
+                    "Portfolio Breakdown\n" + portfolioBreakdown;
 
         }
     }
