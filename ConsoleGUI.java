82cf1571d259547b049b6ed03f2b2bc7fefe64a5
jackp2309 <jackp2309@gmail.com>
2025-05-09 15:22:43 -0500

    Added comments for Console GUI class

--- ConsoleGUI.java
+++ ConsoleGUI.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class ConsoleGUI {
     public static final Scanner scan = new Scanner(System.in);
 
+    /**
+     * Asks user for risk tolerance using scanner
+     * @return
+     */
     public static String getRiskTolerance() {
         while (true) {
             System.out.print("Enter your risk tolerance (high, medium, low): ");
@@ -14,6 +18,10 @@ public class ConsoleGUI {
         }
     }
 
+    /**
+     * Asks user for risk tolerance using scanner
+     * @return
+     */
     public static long getAnnualIncome() {
         while (true) {
             System.out.print("Enter your annual income: ");
@@ -26,6 +34,10 @@ public class ConsoleGUI {
         }
     }
 
+    /**
+     * Asks user for filing status using scanner
+     * @return
+     */
     public static String getFilingStatus() {
         while (true) {
             System.out.print("Filing status (single or married): ");
@@ -35,6 +47,10 @@ public class ConsoleGUI {
         }
     }
 
+    /**
+     * Asks user for time horizon using scanner
+     * @return
+     */
     public static double getTimeHorizon() {
         while (true) {
             System.out.print("Time horizon in years: ");
@@ -47,6 +63,10 @@ public class ConsoleGUI {
         }
     }
 
+    /**
+     * Asks user for monthly expenses using scanner
+     * @return
+     */
     public static double getMonthlyExpenses() {
         while (true) {
             System.out.print("Enter your monthly expenses: ");
